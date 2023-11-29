ccc8cab3dfcd8a74557d09cd4ae4fe0604a9f064
Om Gore <omgore4307@gmail.com>
2023-11-30 00:47:09 +0530

    first commit

---
+++ fibbo.sh
@@ -0,0 +1,20 @@
+echo "Enter the number of terms in the Fibonacci series: " 
+read n
+
+
+a=0
+b=1
+
+echo "Fibonacci Series:"
+echo -n "$a, $b"
+
+
+for ((i=2; i<n; i++)); do
+    next=$((a + b))
+    echo -n ", $next"
+    a=$b
+    b=$next
+done
+
+echo "" 
+
---
+++ rev_string.sh
@@ -0,0 +1,12 @@
+read -p "Enter a string: " input_string
+
+length=${#input_string}
+echo $length
+reversed=""
+
+for (( i=$length-1; i>=0; i-- )); do
+    reversed="${reversed} ${input_string:$i:1}"
+done
+
+echo "Reversed string: $reversed"
+
